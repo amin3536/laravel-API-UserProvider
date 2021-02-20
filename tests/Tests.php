8c270abdbcec925a512dc05847dc2e1d58e9fc53
amin mirmahmoudi <am.a9173@gmail.com>
2021-02-20 11:42:01 +0330

    #add sample test

---
+++ tests/Tests.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+class Tests extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
