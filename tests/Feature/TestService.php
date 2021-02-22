17a6eeaaf994d314434da8ed5290bcc00439e105
amin mirmahmoudi <am.a9173@gmail.com>
2021-02-22 10:55:01 +0330

    # remove unnecessary file in test

--- tests/Feature/TestService.php
+++ tests/Feature/TestService.php
@@ -1,16 +1,15 @@
 <?php
 
 
-use Amin3536\LaravelApiUserProvider\authService\DeserializerInterface;
 
 class TestService extends \Tests\TestCase
 {
 
     public function testProvider()
     {
-        $this->app->resolved(DeserializerInterface::class, function (DeserializerInterface $deserializer) {
-            dump($deserializer);
-        });
+//        $this->app->resolved(DeserializerInterface::class, function (DeserializerInterface $deserializer) {
+//            dump($deserializer);
+//        });
 
     }
 
